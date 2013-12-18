63bdafa0f4fb4200a15992f232a0e33696aceb80
vladmihalcea <mih_vlad@yahoo.com>
2013-12-19 00:39:41 +0200

    use one hour report, instead of middle hour

--- mongodb-facts/aggregator/timeseries/aggregate_hour_report.js
+++ mongodb-facts/aggregator/timeseries/aggregate_hour_report.js
@@ -2,6 +2,7 @@ var minDate = new Date(2012, 0, 1, 0, 0, 0, 0);
 var maxDate = new Date(2013, 0, 1, 0, 0, 0, 0);
 var delta = maxDate.getTime() - minDate.getTime();
 var fromDate = new Date(minDate.getTime() + Math.random() * delta);
+fromDate.setHours(0, 0, 0, 0);
 var toDate = new Date(fromDate.getTime() + 60 * 60 * 1000);
 
 print("Aggregating from " + fromDate + " to " + toDate);
