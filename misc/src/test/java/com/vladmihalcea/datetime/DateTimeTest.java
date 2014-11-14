140f5b53dd11622e328fd0a3239ba346645382b7
vladmihalcea <mih_vlad@yahoo.com>
2014-11-14 14:51:10 +0200

    Add more tests for handling time zones

--- misc/src/test/java/com/vladmihalcea/datetime/DateTimeTest.java
+++ misc/src/test/java/com/vladmihalcea/datetime/DateTimeTest.java
@@ -1,13 +1,20 @@
 package com.vladmihalcea.datetime;
 
-import org.joda.time.DateTime;
+import org.joda.time.*;
+import org.joda.time.chrono.ISOChronology;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.concurrent.TimeUnit;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * DateTimeTest - Date and Time Test
@@ -18,8 +25,52 @@ public class DateTimeTest {
 
     private Logger LOGGER = LoggerFactory.getLogger(DateTimeTest.class);
 
-    // ISO 8601
-    // http://www.w3.org/TR/NOTE-datetime
+    @Test
+    public void testTimeZonesWithCalendar() throws ParseException {
+        assertEquals(0L, newCalendarInstanceMillis("GMT").getTimeInMillis());
+        assertEquals(TimeUnit.HOURS.toMillis(-9), newCalendarInstanceMillis("Japan").getTimeInMillis());
+        assertEquals(TimeUnit.HOURS.toMillis(10), newCalendarInstanceMillis("Pacific/Honolulu").getTimeInMillis());
+        Calendar epoch = newCalendarInstanceMillis("GMT");
+        epoch.setTimeZone(TimeZone.getTimeZone("Japan"));
+        assertEquals(TimeUnit.HOURS.toMillis(-9), epoch.getTimeInMillis());
+    }
+
+    private Calendar newCalendarInstanceMillis(String timeZoneId) {
+        Calendar calendar = new GregorianCalendar();
+        calendar.set(Calendar.YEAR, 1970);
+        calendar.set(Calendar.MONTH, 0);
+        calendar.set(Calendar.DAY_OF_MONTH, 1);
+        calendar.set(Calendar.HOUR_OF_DAY, 0);
+        calendar.set(Calendar.MINUTE, 0);
+        calendar.set(Calendar.SECOND, 0);
+        calendar.set(Calendar.MILLISECOND, 0);
+        calendar.setTimeZone(TimeZone.getTimeZone(timeZoneId));
+        return calendar;
+    }
+
+    @Test
+    public void testTimeZonesWithDateTime() throws ParseException {
+        assertEquals(0L, newDateTimeMillis("GMT").toDate().getTime());
+        assertEquals(TimeUnit.HOURS.toMillis(-9), newDateTimeMillis("Japan").toDate().getTime());
+        assertEquals(TimeUnit.HOURS.toMillis(10), newDateTimeMillis("Pacific/Honolulu").toDate().getTime());
+        DateTime epoch = newDateTimeMillis("GMT");
+        assertEquals("1970-01-01T00:00:00.000Z", epoch.toString());
+        epoch = epoch.toDateTime(DateTimeZone.forID("Japan"));
+        assertEquals(0, epoch.toDate().getTime());
+        assertEquals("1970-01-01T09:00:00.000+09:00", epoch.toString());
+        MutableDateTime mutableDateTime = epoch.toMutableDateTime();
+        mutableDateTime.setChronology(ISOChronology.getInstance().withZone(DateTimeZone.forID("Japan")));
+        assertEquals("1970-01-01T09:00:00.000+09:00", epoch.toString());
+    }
+
+    private DateTime newDateTimeMillis(String timeZoneId) {
+        return new DateTime(DateTimeZone.forID(timeZoneId))
+                .withYear(1970)
+                .withMonthOfYear(1)
+                .withDayOfMonth(1)
+                .withTimeAtStartOfDay();
+    }
+
     @Test
     public void testSimpleDateFormatterWithUTCStrings() throws ParseException {
         dateFormatParse("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'", "1970-01-01T00:00:00.200Z", 200L);
