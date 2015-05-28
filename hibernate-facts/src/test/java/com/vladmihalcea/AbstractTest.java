731ca754d7e69854a6140ad237f5be6141ecc9c2
vladmihalcea <mih_vlad@yahoo.com>
2015-05-29 00:25:41 +0300

    Simplify test and check TRANSACTIONAL cache concurrency transaction isolation

---
+++ hibernate-facts/src/test/java/com/vladmihalcea/AbstractTest.java
@@ -0,0 +1,154 @@
+package com.vladmihalcea;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.support.TransactionTemplate;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.*;
+
+public abstract class AbstractTest {
+
+    static {
+        Thread.currentThread().setName("Alice");
+    }
+
+    protected enum LockType {
+        LOCKS,
+        MVLOCKS,
+        MVCC
+    }
+
+    private final ExecutorService executorService = Executors.newSingleThreadExecutor(r -> {
+        Thread bob = new Thread(r);
+        bob.setName("Bob");
+        return bob;
+    });
+
+    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
+
+    @FunctionalInterface
+    protected interface VoidCallable extends Callable<Void> {
+
+        void execute();
+
+        default Void call() throws Exception {
+            execute();
+            return null;
+        }
+    }
+
+    @FunctionalInterface
+    protected interface EntityManagerCallable<T> {
+        T execute(EntityManager entityManager);
+    }
+
+    @FunctionalInterface
+    protected interface EntityManagerVoidCallable {
+        void execute(EntityManager entityManager);
+    }
+
+    @FunctionalInterface
+    protected interface TransactionCallable<T> extends EntityManagerCallable<T> {
+        default void beforeTransactionCompletion() {
+
+        }
+
+        default void afterTransactionCompletion() {
+
+        }
+    }
+
+    @FunctionalInterface
+    protected interface TransactionVoidCallable extends EntityManagerVoidCallable {
+    }
+
+    @PersistenceContext(unitName = "persistenceUnit")
+    private EntityManager entityManager;
+
+    @Autowired
+    private TransactionTemplate transactionTemplate;
+
+    protected <T> T doInTransaction(TransactionCallable<T> callable) {
+        return transactionTemplate.execute(status -> callable.execute(entityManager));
+    }
+
+    protected void doInTransaction(TransactionVoidCallable callable) {
+        transactionTemplate.execute(status -> {
+            callable.execute(entityManager);
+            return null;
+        });
+    }
+
+    protected void executeSync(VoidCallable callable) {
+        executeSync(Collections.singleton(callable));
+    }
+
+    protected void executeSync(Collection<VoidCallable> callables) {
+        try {
+            List<Future<Void>> futures = executorService.invokeAll(callables);
+            for (Future<Void> future : futures) {
+                future.get();
+            }
+        } catch (InterruptedException | ExecutionException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    protected <T> void executeAsync(Runnable callable, final Runnable completionCallback) {
+        final Future future = executorService.submit(callable);
+        new Thread(() -> {
+            while (!future.isDone()) {
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+            try {
+                completionCallback.run();
+            } catch (Exception e) {
+                throw new IllegalStateException(e);
+            }
+        }).start();
+    }
+
+    protected Future<?> executeAsync(Runnable callable) {
+        return executorService.submit(callable);
+    }
+
+    protected LockType lockType() {
+        return LockType.LOCKS;
+    }
+
+    protected void awaitOnLatch(CountDownLatch latch) {
+        try {
+            latch.await();
+        } catch (InterruptedException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    protected void sleep(int millis) {
+        sleep(millis, null);
+    }
+
+    protected <V> V sleep(int millis, Callable<V> callable) {
+        V result = null;
+        try {
+            LOGGER.info("Wait {} ms!", millis);
+            if (callable != null) {
+                result = callable.call();
+            }
+            Thread.sleep(millis);
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        }
+        return result;
+    }
+}
--- hibernate-facts/src/test/java/com/vladmihalcea/HibernateCacheTest.java
+++ hibernate-facts/src/test/java/com/vladmihalcea/HibernateCacheTest.java
@@ -33,26 +33,23 @@ import org.springframework.transaction.support.TransactionTemplate;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:spring/applicationContext-test.xml"})
 @DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_CLASS)
-public class HibernateCacheTest {
-
-    protected final Logger LOGGER = LoggerFactory.getLogger(getClass());
-
-    @PersistenceContext(unitName = "persistenceUnit")
-    private EntityManager entityManager;
-
-    @Autowired
-    private TransactionTemplate transactionTemplate;
+public class HibernateCacheTest extends AbstractTest {
 
     private Repository repositoryReference;
 
     @Before
     public void init() {
-        repositoryReference = transactionTemplate.execute(transactionStatus -> {
+        repositoryReference = doInTransaction((entityManager) -> {
             LOGGER.info("Transactional entities are write-through on persisting");
 
             Repository repository = new Repository("Hibernate-Master-Class");
@@ -72,7 +69,7 @@ public class HibernateCacheTest {
     @Test
     public void testRepositoryEntityUpdate() {
         LOGGER.info("Read-write entities are write-through on updating");
-        transactionTemplate.execute(transactionStatus -> {
+        doInTransaction((entityManager) -> {
             Repository repository = entityManager.find(Repository.class, repositoryReference.getId());
             repository.setName("High-Performance Hibernate");
             for (Commit commit : repository.getCommits()) {
@@ -80,27 +77,49 @@ public class HibernateCacheTest {
                     assertNotNull(change.getDiff());
                 }
             }
-            return null;
         });
-        transactionTemplate.execute(transactionStatus -> {
+        doInTransaction((entityManager) -> {
             LOGGER.info("Reload entity after updating");
             Repository repository = entityManager.find(Repository.class, repositoryReference.getId());
             assertEquals("High-Performance Hibernate", repository.getName());
-            return null;
         });
     }
 
     @Test
     public void testRepositoryEntityDelete() {
         LOGGER.info("Read-write entities are deletable");
-        transactionTemplate.execute(transactionStatus -> {
+        doInTransaction((entityManager) -> {
             Repository repository = entityManager.find(Repository.class, repositoryReference.getId());
             entityManager.remove(repository);
-            return null;
         });
-        transactionTemplate.execute(transactionStatus -> {
+        doInTransaction((entityManager) -> {
             assertNull(entityManager.find(Repository.class, repositoryReference.getId()));
-            return null;
+        });
+    }
+
+    @Test
+    public void testIsolationLevel() {
+        LOGGER.info("Read-write entities are deletable");
+        doInTransaction((entityManager) -> {
+            Repository repository = entityManager.find(Repository.class, repositoryReference.getId());
+            executeSync(() -> {
+                doInTransaction(_entityManager -> {
+                    Repository _repository = entityManager.find(Repository.class, repositoryReference.getId());
+                    _repository.setName("High-Performance Hibernate");
+                    LOGGER.info("Updating repository name to {}", _repository.getName());
+                });
+            });
+
+            repository = entityManager.find(Repository.class, repositoryReference.getId());
+            assertEquals("Hibernate-Master-Class", repository.getName());
+
+            LOGGER.info("Detaching repository ");
+            entityManager.detach(repository);
+            assertFalse(entityManager.contains(repository));
+
+            repository = entityManager.find(Repository.class, repositoryReference.getId());
+
+            assertEquals("High-Performance Hibernate", repository.getName());
         });
     }
 
